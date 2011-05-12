5916f6ccee0b2f3eef60ad757beed892c2845466
stevecreedon <stephen.creedon@rapidappsgroup.com>
2011-05-12 21:10:43 +0100

    first rails commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lightartadmin::Application.config.secret_token = '8a5f563d7098d4a2f379fa6a878a9448022c3c17ac4b6704b98ca2d0ca61633498faa765388f9c601571e95c687b39c9d2c4bcea90e5a5ef1df47c558b3e9d25'
