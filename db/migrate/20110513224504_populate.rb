1410c117874206c80807d1948e7aebc600f800ff
stevecreedon <stephen.creedon@rapidappsgroup.com>
2011-05-14 09:59:52 +0100

    created objects first dploy

--- db/migrate/20110513224504_populate.rb
+++ db/migrate/20110513224504_populate.rb
@@ -1,8 +1,28 @@
 class Populate < ActiveRecord::Migration
   def self.up
-    CostCentre.create!("")
+    CostCentre.create!(:name => "21st Century Tiger")
+    CostCentre.create!(:name => "Light Art R&D")
+    CostCentre.create!(:name => "Light Art Business")
+    
+    PaymentType.create!(:name => "Cash")
+    PaymentType.create!(:name => "Steve, Visa Debit")
+    PaymentType.create!(:name => "Steve, Amex")
+    
+    CostType.create!(:name => "stock")
+    CostType.create!(:name => "modelling materials")
+    CostType.create!(:name => "postage")
+    CostType.create!(:name => "PPC")
+    CostType.create!(:name => "website")
+    
+    Company.create!(:name => "SKK")
+    Company.create!(:name => "21st Century Tiger")
+    Company.create!(:name => "Heico")
   end
 
   def self.down
+    CostCentre.delete_all
+    Company.delete_all
+    PaymentType.delete_all
+    CostType.delete_all
   end
 end
