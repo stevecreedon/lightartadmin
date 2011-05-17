a3aece01d2be169163c9d86a1a66e29ddddc2eb5
stevecreedon <stephen.creedon@rapidappsgroup.com>
2011-05-17 12:37:47 +0100

    fixed spent and last select boxes on the index page

--- app/helpers/costs_helper.rb
+++ app/helpers/costs_helper.rb
@@ -4,17 +4,17 @@ module CostsHelper
     [[17.5,17.5],[12.5,12.5],[0.0,0.0]]
   end
   
-  def users_options
-    options_for_select([['All', nil]] + User.all.collect{|u| [u.email, u.id.to_s]})
+  def users_options(selected=nil)
+    options_for_select([['All', nil]] + User.all.collect{|u| [u.email, u.id.to_s]}, selected)
   end
   
-  def in_the_last_options
+  def in_the_last_options(selected=nil)
     options_for_select([
   		["this week","0"], 
   		["in the last 7 days","1"], 
   		["this month","2"], 
   		["the last 30 days","3"]
-  	])
+  	], selected)
   end
   
 end
